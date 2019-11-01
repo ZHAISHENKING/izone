30a1176ae51b12d4f92e81283ce8a26045e8870e
ZHAISHENKING <18700790825@163.com>
2019-11-01 14:50:01 +0800

    修改启动文件

--- manage.py
+++ manage.py
@@ -7,6 +7,11 @@ db.create_all(app=app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
-
+manager.add_command("runserver",
+                    Server(
+                        host='0.0.0.0',
+                        port=5000,
+                        use_debugger=False
+                    ))
 if __name__ == '__main__':
     manager.run()
